ee6cd5fb15673f5c57ea6dc4af171deb9b8efb77
Quy Tran <quy.tran@bluecomgroup.com>
2016-11-30 18:52:59 +0700

    [PAC-5] Set dynamic customer group - Calculate age of customer for every request if they logged in then put it into session.custom.userAge - Create customer group depend on userAge session

---
+++ app_pacsafe_core/cartridge/scripts/util/TimeHelper.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * Get age from given day of birth
+ *
+ * @param birthday {Date}
+ *
+ **/
+function getAge (birthday) {
+	var age = -1;
+	try {
+		let Calendar = require('dw/util/Calendar');
+		
+	    let now = new Calendar(new Date());
+	    let dob = new Calendar(birthday);
+	    
+	    let year1 = now.get(Calendar.YEAR);
+	    let year2 = dob.get(Calendar.YEAR);
+		age = year1 - year2;
+		let month1 = now.get(Calendar.MONTH);
+		let month2 = dob.get(Calendar.MONTH);
+		if (month2 > month1) {
+			age--;
+		} else if (month1 == month2) {
+			var day1 = now.get(Calendar.DAY_OF_MONTH);
+			var day2 = dob.get(Calendar.DAY_OF_MONTH);
+			if (day2 > day1) {
+				age--;
+			}
+		}
+	} catch (e) {
+		response.getWriter().println('Error when running TimeHelper.js, function getAge. Error: ' + e);
+	}
+	
+	return age;
+}
+
+module.exports = {
+	getAge : getAge
+}
