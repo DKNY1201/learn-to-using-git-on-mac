4eb354f630b6d5293ae05c09e0127a03894ff4df
Vu Nguyen <vu.nguyen@bluecomgroup.com>
2016-12-07 11:44:44 +0700

    [PAC-3] Update header.js file using .on event

--- app_pacsafe_core/cartridge/js/header.js
+++ app_pacsafe_core/cartridge/js/header.js
@@ -1,78 +1,39 @@
 'use strict';
 
 function initializeEvents() {
-	debugger
-    // Show/Hide navigation category menu on shop link hover
-	$cache.headerShopLinkCatMenu.on( "hover",
-		function() {
-			$cache.navCategoryMenu.stop().fadeIn();
-	        $cache.headerShopLink.css({
-	            'background-color': '#201547',
-	            'color': '#fff'
-	        });
-	    },
-	    function() {
-	        $cache.navCategoryMenu.stop().fadeOut();
-	        $cache.headerShopLink.css({
-	            'background-color': '#fff',
-	            'color': '#201547'
-	        });
-	    }
-	);
-	
-	
-//    $cache.headerShopLinkCatMenu.hover(
-//        function() {
-//            $cache.navCategoryMenu.stop().fadeIn();
-//            $cache.headerShopLink.css({
-//                'background-color': '#201547',
-//                'color': '#fff'
-//            });
-//        },
-//        function() {
-//            $cache.navCategoryMenu.stop().fadeOut();
-//            $cache.headerShopLink.css({
-//                'background-color': '#fff',
-//                'color': '#201547'
-//            });
-//        }
-//    );
-	
+    // Show/Hide navigation category menu on shop link hover	
+    $cache.headerShopLinkCatMenu.on("mouseenter", function(event) {
+        $cache.navCategoryMenu.stop().fadeIn();
+        $cache.headerShopLink.css({
+            'background-color': '#201547',
+            'color': '#fff'
+        });
+    });
+
+    $cache.headerShopLinkCatMenu.on("mouseleave", function(event) {
+        $cache.navCategoryMenu.stop().fadeOut();
+        $cache.headerShopLink.css({
+            'background-color': '#fff',
+            'color': '#201547'
+        });
+    });
+
     // Show/Hide navigation library menu on explore link hover
-	
-	$cache.headerExploreLinkLibraryMenu.on( "hover",
-		function() {
-	        $cache.navLibraryMenu.stop().fadeIn();
-	        $cache.headerExploreLink.css({
-	            'background-color': '#201547',
-	            'color': '#fff'
-	        });
-	    },
-	    function() {
-	        $cache.navLibraryMenu.stop().fadeOut();
-	        $cache.headerExploreLink.css({
-	            'background-color': '#fff',
-	            'color': '#201547'
-	        });
-	    }
-	);
-	
-//    $cache.headerExploreLinkLibraryMenu.hover(
-//        function() {
-//            $cache.navLibraryMenu.stop().fadeIn();
-//            $cache.headerExploreLink.css({
-//                'background-color': '#201547',
-//                'color': '#fff'
-//            });
-//        },
-//        function() {
-//            $cache.navLibraryMenu.stop().fadeOut();
-//            $cache.headerExploreLink.css({
-//                'background-color': '#fff',
-//                'color': '#201547'
-//            });
-//        }
-//    );
+    $cache.headerExploreLinkLibraryMenu.on("mouseenter", function(event) {
+        $cache.navLibraryMenu.stop().fadeIn();
+        $cache.headerExploreLink.css({
+            'background-color': '#201547',
+            'color': '#fff'
+        });
+    });
+
+    $cache.headerExploreLinkLibraryMenu.on("mouseleave", function(event) {
+        $cache.navLibraryMenu.stop().fadeOut();
+        $cache.headerExploreLink.css({
+            'background-color': '#fff',
+            'color': '#201547'
+        });
+    });
 }
 
 var $cache = {};
